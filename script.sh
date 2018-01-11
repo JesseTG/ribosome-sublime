f0e6494e8ad728da6de43b329bf1d1676b55577d
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-01-11 16:08:02 -0500

    Use the project file

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 set -e
-xvfb-run subl ./Ribosome-Python.sublime-syntax.yaml-macros
+xvfb-run subl "./Ribosome.sublime-project"
+subl -b --command 'open_file {"file": "$project_path/Ribosome-Python.sublime-syntax.yaml-macros"}'
 subl -b --command 'set_build_system {"file": "Packages/YAMLMacros/YAML Macros.sublime-build"}'
 subl -b --command 'build'
 ls
