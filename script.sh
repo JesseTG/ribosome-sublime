02457f77216fc1ad15cf60b9a8f9e5bc504ca500
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-01-11 15:35:10 -0500

    Will this get me the results I want?

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 xvfb-run subl ./Ribosome-Python.sublime-syntax.yaml-macros
 subl -b --command 'show_panel {"panel": "console", "toggle": true}'
-python3 ./YAMLMacros/cli/cli.py < ./Ribosome-Python.sublime-syntax.yaml-macros
+subl -b --command 'set_build_system {"file": "Packages/YAMLMacros/YAML Macros.sublime-build"}'
+subl -b --command 'build'
+ls
