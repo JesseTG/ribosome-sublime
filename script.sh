61db2afe88486cf9984e004a78c1a3bb8c765de9
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-01-11 15:43:45 -0500

    Try this

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
+set -e
 xvfb-run subl ./Ribosome-Python.sublime-syntax.yaml-macros
-subl -b --command 'show_panel {"panel": "console", "toggle": true}'
 subl -b --command 'set_build_system {"file": "Packages/YAMLMacros/YAML Macros.sublime-build"}'
 subl -b --command 'build'
 ls
