22399a07f7957d026cf37bf2f228ebe6edef2efb
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-01-12 12:07:30 -0500

    ps u --cols 120

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 set -e
 subl --project "./Ribosome.sublime-project"
-ps u
+ps u --cols 120
 subl -b --command 'open_file {"file": "$project_path/Ribosome-Python.sublime-syntax.yaml-macros"}'
-ps u
+ps u --cols 120
 subl -b --command 'set_build_system {"file": "Packages/YAMLMacros/YAML Macros.sublime-build"}'
-ps u
+ps u --cols 120
 subl -b --command 'build'
-ps u
+ps u --cols 120
 ls
