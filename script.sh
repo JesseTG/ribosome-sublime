2fb8fe25aadc014b385af788194ed14b1b7353c7
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-01-12 11:55:03 -0500

    Let's put on my detective cap

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 set -e
-xvfb-run subl "./Ribosome.sublime-project"
+xvfb-run subl --project "./Ribosome.sublime-project"
+ps u
 subl -b --command 'open_file {"file": "$project_path/Ribosome-Python.sublime-syntax.yaml-macros"}'
+ps u
 subl -b --command 'set_build_system {"file": "Packages/YAMLMacros/YAML Macros.sublime-build"}'
+ps u
 subl -b --command 'build'
+ps u
 ls
